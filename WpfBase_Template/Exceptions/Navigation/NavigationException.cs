7211c22531e37d543b74922c4a3fde4e5624ae3c
Philipp Siewert <philipp@phsiewert.de>
2024-08-08 07:38:01 +0200

    Added comments to Exception classes.

--- WpfBase_Template/Exceptions/Navigation/NavigationException.cs
+++ WpfBase_Template/Exceptions/Navigation/NavigationException.cs
@@ -1,31 +1,61 @@
-﻿///TODO: Add comments
+﻿namespace WpfBase_Template.Exceptions.Navigation;
 
-namespace WpfBase_Template.Exceptions.Navigation;
-
+/// <summary>
+/// Navigation exception base class.
+/// </summary>
 public abstract class NavigationException : Exception
 {
+    /// <summary>
+    /// Page key used when exception occured.
+    /// </summary>
     private string? _pageKey;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NavigationException"/> class.
+    /// </summary>
     public NavigationException()
     { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NavigationException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message</param>
     public NavigationException(string message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NavigationException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Inner exception.</param>
     public NavigationException(string message, Exception? innerException) : base(message, innerException)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NavigationException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message</param>
+    /// <param name="pageKey">Page key.</param>
     public NavigationException(string message, string pageKey) : this(message, null, pageKey)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NavigationException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Inner exception.</param>
+    /// <param name="pageKey">Page key.</param>
     public NavigationException(string message, Exception? innerException, string pageKey)
         : base(message, innerException)
     {
         _pageKey = pageKey;
     }
 
+    /// <summary>
+    /// Gets the page key.
+    /// </summary>
     public string PageKey => _pageKey ??= string.Empty;
 }
--- WpfBase_Template/Exceptions/Navigation/PageServiceException.cs
+++ WpfBase_Template/Exceptions/Navigation/PageServiceException.cs
@@ -1,24 +1,48 @@
-﻿///TODO: Add comments
+﻿namespace WpfBase_Template.Exceptions.Navigation;
 
-namespace WpfBase_Template.Exceptions.Navigation;
-
+/// <summary>
+/// PageService exception class.
+/// </summary>
 public class PageServiceException : NavigationException
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageServiceException"/> class.
+    /// </summary>
     public PageServiceException()
     { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageServiceException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
     public PageServiceException(string message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageServiceException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Inner exception.</param>
     public PageServiceException(string message, Exception? innerException) : base(message, innerException)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageServiceException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="pageKey">Page key.</param>
     public PageServiceException(string message, string pageKey) : this(message, null, pageKey)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageServiceException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Inner exception.</param>
+    /// <param name="pageKey">Page key.</param>
     public PageServiceException(string message, Exception? innerException, string pageKey)
         : base(message, innerException, pageKey) { }
 }
